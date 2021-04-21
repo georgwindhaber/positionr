4958115743d5ad29748d0b22525418c4a9ecce0f
Georg Windhaber <georg.windhaber@gmail.com>
2021-04-21 17:44:27 +0200

    Made router routing

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="container mx-auto h-full bg-gray-50">
-    <div v-if="site == 'list'" class="positions flex-col p-3">
+    <div class="positions flex-col p-3">
       <button
         v-for="(position, index) of positions"
         :key="index"
-        @click="openPosition(position)"
         class="w-full rounded shadow-md focus:shadow focus:outline-none mt-3 p-3 bg-white"
       >
-        <h2 class="text-xl font-bold">{{ position.title }}</h2>
+        <router-link :to="'/position/' + position.id">
+          <h2 class="text-xl font-bold">{{ position.title }}</h2>
+        </router-link>
         <p>{{ position.excerpt }}</p>
       </button>
     </div>
-    <position v-if="site == 'position'" :position="activePosition" />
+    <router-link to="/">Go to Home</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Position from "./pages/Position/Position.vue";
 
 export default defineComponent({
   name: "App",
   data() {
     return {
-      site: "list",
       positions: [
-        { title: "Radwege", excerpt: "Lorem ipsum dolor sit amet, consectuor" },
         {
+          id: 1,
+          title: "Radwege",
+          excerpt: "Lorem ipsum dolor sit amet, consectuor",
+        },
+        {
+          id: 2,
           title: "Bedingungsloses Grundeinkommen",
           excerpt: "Jeder soll leben können und das fair",
         },
       ],
-      activePosition: null,
     };
   },
-  methods: {
-    openPosition(position) {
-      this.activePosition = position
-      this.site = "position"
-    }
-  },
-  components: { Position },
+  methods: {},
 });
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
 import App from "./App.vue";
+import Root from "./Root.vue";
+import Position from "./pages/Position/Position.vue";
 import "./index.css";
 
-createApp(App).mount("#app");
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+
+const history = createWebHistory();
+const routes = [
+  { path: "/", component: App },
+  { path: "/position/:id", component: Position },
+];
+const router = createRouter({
+  history,
+  routes,
+});
+
+createApp(Root).use(router).mount("#app");
