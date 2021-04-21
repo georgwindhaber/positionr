a4b091444bf2bbd6dcb8b9ab0ee01bc68eb2b197
Georg Windhaber <georg.windhaber@gmail.com>
2021-04-21 16:52:30 +0200

    Added provisorial router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="container mx-auto h-full bg-gray-50">
-    <div class="positions flex-col p-3">
+    <div v-if="site == 'list'" class="positions flex-col p-3">
       <button
         v-for="(position, index) of positions"
         :key="index"
+        @click="openPosition(position)"
         class="w-full rounded shadow-md focus:shadow focus:outline-none mt-3 p-3 bg-white"
       >
         <h2 class="text-xl font-bold">{{ position.title }}</h2>
         <p>{{ position.excerpt }}</p>
       </button>
     </div>
+    <position v-if="site == 'position'" :position="activePosition" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Position from "./pages/Position/Position.vue";
 
 export default defineComponent({
   name: "App",
   data() {
     return {
+      site: "list",
       positions: [
         { title: "Radwege", excerpt: "Lorem ipsum dolor sit amet, consectuor" },
         {
@@ -28,11 +31,16 @@ export default defineComponent({
           excerpt: "Jeder soll leben können und das fair",
         },
       ],
+      activePosition: null,
     };
   },
-  components: {
-    HelloWorld,
+  methods: {
+    openPosition(position) {
+      this.activePosition = position
+      this.site = "position"
+    }
   },
+  components: { Position },
 });
 </script>
 
---
+++ src/pages/Position/Position.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="p-3">
+    <div
+      class="position w-full rounded shadow-md focus:shadow focus:outline-none p-3 bg-white"
+    >
+      <h1 class="text-xl font-bold">{{ position.title }}</h1>
+      <p>{{ position.text }}</p>
+      <button @click="voteYes()">Ja!</button>
+      <button @click="voteNo()">Nein!</button>
+      <button @click="goBack()">Zurück</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      type: Object,
+      default: null,
+    },
+  },
+  methods: {
+    goBack() {
+      this.$root.site = "list";
+    },
+  },
+};
+</script>
+
+<style></style>
